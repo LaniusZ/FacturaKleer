9cc604ac71c14c5fc58ecfc91e7ac9e86298a933
Alfonso Alamos <alfonso.informatico@gmail.com>
2020-08-24 14:05:08 -0400

    Crear Esqueleto

--- factura.rb
+++ factura.rb
@@ -1,1 +1,16 @@
-puts "Hola Mundo"
+class FacturacionKleer
+    def initialize
+        
+    end
+
+    def calcular(cantidad, precio, estado)
+        puts "Sub Total            : "
+        puts "Impuesto (Estado: AL): "
+        puts "Descuento            : "
+        puts "Total                : "
+    end
+end
+
+fact = FacturacionKleer.new
+
+fact.calcular(ARGV[0], ARGV[1], ARGV[2])
