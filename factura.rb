861e5f735754f9b8d47bbba1612cee872a919fee
Alfonso Alamos <alfonso.informatico@gmail.com>
2020-08-24 14:09:31 -0400

    Validacion de parametros nulos base

--- factura.rb
+++ factura.rb
@@ -4,10 +4,21 @@ class FacturacionKleer
     end
 
     def calcular(cantidad, precio, estado)
+        validarParam(cantidad, "cantidad")
+        validarParam(precio, "precio unitario")
+        validarParam(estado, "estado")
+
         puts "Sub Total            : "
         puts "Impuesto (Estado: AL): "
         puts "Descuento            : "
         puts "Total                : "
+    end
+
+    def validarParam(param, name)
+        if(param == nil)
+            puts "Debe completar el parametro #{name}"
+            exit 1
+        end
     end
 end
 
